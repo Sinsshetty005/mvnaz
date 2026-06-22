234db781f3da22dfecfc37e21408ad9503892bc4
Sinsshetty005 <sinchushetty005@gmail.com>
2026-06-22 22:03:36 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/sinsjava/App.java
@@ -0,0 +1,13 @@
+package com.sinsjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
